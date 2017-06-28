169778f881ac04b3105253d4727b304e3d2a874c
AaronDLP <aaron@doublelinepartners.com>
2017-06-28 09:09:33 -0500

    update to .net core

---
+++ Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Program
+    {
+        /// <summary>
+        /// Solves Sudoku puzzles from command line in parallel.
+        /// <example>000075400000000008080190000300001060000000034000068170204000603900000020530200000</example>
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var puzzles = new List<string>();
+            var strGrid = System.Console.ReadLine();
+            while (!string.IsNullOrEmpty(strGrid))
+            {
+                puzzles.Add(strGrid);
+                strGrid = System.Console.ReadLine();
+            }
+            var sudoku = new Library.Sudoku();
+            Parallel.ForEach(puzzles, s => System.Console.WriteLine(sudoku.Solve(s) ?? "no solution"));
+            System.Console.WriteLine("{0} solutions per second", 10000.0 / stopwatch.Elapsed.TotalSeconds);
+        }
+    }
+}
--- Library/Sudoku.cs
+++ Library/Sudoku.cs
@@ -1,33 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Sudoku
+namespace Library
 {
-    class Program
+    public class Sudoku
     {
-        /// <summary>
-        /// Solves Sudoku puzzles from command line in parallel.
-        /// <example>000075400000000008080190000300001060000000034000068170204000603900000020530200000</example>
-        /// </summary>
-        static void Main(string[] args)
-        {
-            var stopwatch = Stopwatch.StartNew();
-            var puzzles = new List<string>();
-            var strGrid = Console.ReadLine();
-            while (!string.IsNullOrEmpty(strGrid))
-            {
-                puzzles.Add(strGrid);
-                strGrid = Console.ReadLine();
-            }
-            Parallel.ForEach(puzzles, s => Console.WriteLine(Solve(s) ?? "no solution"));
-            Console.WriteLine("{0} solutions per second", 10000.0 / stopwatch.Elapsed.TotalSeconds);
-        }
-
-        private static string Solve(string s)
+        public string Solve(string s)
         {
             var builder = new StringBuilder(s);
             for (var i = 0; i < 81; i++)
@@ -39,7 +18,7 @@ namespace Sudoku
             return Solve(builder.ToString(), 0);
         }
 
-        private static string Solve(string s, int p)
+        private string Solve(string s, int p)
         {
             if (p == 81) return s;
             if (s[p] != '0') return Solve(s, p + 1);
@@ -53,7 +32,7 @@ namespace Sudoku
             return null;
         }
 
-        private static IEnumerable<char> UnusedValues(string a, int p)
+        private IEnumerable<char> UnusedValues(string a, int p)
         {
             var result = new bool[] { false, true, true, true, true, true, true, true, true, true };
             for (var i = 0; i < 9; i++)
