8fc70c164f811e6a857dc2a87ac5d7251010be00
Aaron Wells <aaron@incwells.com>
2021-07-22 22:24:13 -0600

    Update to .NET 6

--- SudokuWeb/Startup.cs
+++ SudokuWeb/Startup.cs
@@ -25,7 +25,6 @@ namespace SudokuWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
             {
