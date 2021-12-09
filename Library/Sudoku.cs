87127abbd1442def9f27b8a6f8ea381982829893
Aaron Wells <aaron@incwells.com>
2021-12-08 19:40:34 -0700

    Added comments to library

--- Library/Sudoku.cs
+++ Library/Sudoku.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace Library
 {
+    /*
+        This class solves a soduku puzzle in two steps:
+        1) update the puzzle by populating trivial cells (only one unused value)
+        2) solve the puzzle's trivial cells by exhaustive search using a stack
+    */
     public class Sudoku
     {
+        /*
+            This method populates the trivial cells then passes off the solution to the exhaustive search
+        */
         public static string Solve(string s)
         {
             var builder = new StringBuilder(s);
@@ -18,6 +26,12 @@ namespace Library
             return Solve(builder.ToString(), 0);
         }
 
+        /*
+            This recursive method exhaustively searches for a solution of to the puzzle
+            by trying each possible value for a cell until it
+            1) finds a value for the last cell and returns the result (success)
+            2) has no possible values for a cell and returns null (failure)
+        */
         private static string Solve(string s, int p)
         {
             if (p == 81) return s;
@@ -32,6 +46,13 @@ namespace Library
             return null;
         }
 
+        /*
+            This method returns an enumerable list of values not found
+            a) in the same row as the cell
+            b) in the same column as the cell
+            c) in the same block (3x3) as the cell
+            This is accomplished with a bit-mask representing the characters from '0' to '9'
+        */
         private static IEnumerable<char> UnusedValues(string a, int p)
         {
             var result = new bool[] { false, true, true, true, true, true, true, true, true, true };
