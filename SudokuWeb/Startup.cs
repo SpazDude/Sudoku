1e4ef1c061ceaa19f9a557ab35f1ab7a28f1fb6a
Aaron Wells <aaron@incwells.com>
2021-12-08 19:59:16 -0700

    Updated IHostingEnvironment to IWebHostEnvironment

--- SudokuWeb/Startup.cs
+++ SudokuWeb/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Text;
@@ -23,7 +24,7 @@ namespace SudokuWeb
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
 
             if (env.IsDevelopment())
